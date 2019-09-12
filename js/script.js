4427cba6e13f716d94e5a9cdcdc99c28a646b8ad
Usha Priyanka Addala <upa8998@gmail.com>
2019-09-12 11:14:50 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -94,7 +94,7 @@ document.getElementById('quote-box').innerHTML = message;
   comment.
 ***/
 
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
